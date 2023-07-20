81363f613245cf95082cfdaa0aacf8032c89e0c8
Snehal <Snehal@DESKTOP-C4V5319>
2023-07-20 14:09:41 +0530

    first commit

---
+++ dayone/src/org/tnsif/introduction/CompilerInput.java
@@ -0,0 +1,11 @@
+package org.tnsif.introduction;
+
+public class CompilerInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short num=45;
+        System.out.println("the num is:"+num);
+	}
+
+}
---
+++ dayone/src/org/tnsif/introduction/UserInputDemo.java
@@ -0,0 +1,15 @@
+package org.tnsif.introduction;
+
+import java.util.Scanner;
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Scanner s=new Scanner(System.in);
+       System.out.println("enter the string:");
+       String str=s.nextLine();
+       System.out.println("string is:"+str);
+	}
+
+}
