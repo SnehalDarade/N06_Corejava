6660169acaa4e535e430a4d1925a28e2efaca26c
Snehal <Snehal@DESKTOP-C4V5319>
2023-09-25 11:54:05 +0530

    first commit

---
+++ DayThirteen/src/org/tnsif/interthreadcommunication/java/InterThreadCommunicationDemo.java
@@ -0,0 +1,26 @@
+package org.tnsif.interthreadcommunication.java;
+
+//Program to demonstrate inter-thread communication
+public class InterThreadCommunicationDemo {
+	public static void main(String[] args) {
+		
+		//Shared Resource
+		Q obj=new Q();
+		
+		Producer producerThread=new Producer(obj); //Producer thread
+		Consumer consumerThread=new Consumer(obj); //Consumer thread
+		
+		
+		  try { producerThread.join(); 
+		  consumerThread.join(); } catch (InterruptedException e) {
+		  System.out.println(e); }
+		 
+		System.out.println("---------End of main()----------");	
+	}
+}
+
+
+
+
+
+
